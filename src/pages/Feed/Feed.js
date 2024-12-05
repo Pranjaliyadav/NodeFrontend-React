119b78f9fc27355bc3f032a524d9110cb9816748
Pranjali Yadav <51490690+Pranjaliyadav@users.noreply.github.com>
2024-12-05 18:29:34 +0530

    corrected create post for image upload

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -105,7 +105,10 @@ class Feed extends Component {
     this.setState({
       editLoading: true
     });
-    // Set up data (with image!)
+  const formData = new FormData()
+  formData.append('title', postData.title)
+  formData.append('content', postData.content)
+  formData.append('image',postData.image)
     let url = 'http://localhost:8080/feed/create-post';
     let method = 'POST'
     if (this.state.editPost) {
@@ -114,13 +117,8 @@ class Feed extends Component {
 
     fetch(url,{
       method,
-      headers : {
-        'Content-Type' : 'application/json' //sendign json data
-      },
-      body : JSON.stringify({
-        title : postData.title,
-        content : postData.content
-      }),
+     
+      body :formData
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
