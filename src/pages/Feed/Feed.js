36b800413662a1d9a6ec5fb115fa2625b810ab64
Pranjali Yadav <51490690+Pranjaliyadav@users.noreply.github.com>
2024-12-10 20:19:20 +0530

    get posts ith pagination resolved

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -55,7 +55,7 @@ class Feed extends Component {
     }
     const graphqlQuery = {
       query:`{
-      getPosts{
+      getPosts(page : ${page}){
       posts{
       _id
       creator{name}
@@ -210,6 +210,7 @@ class Feed extends Component {
             );
             updatedPosts[postIndex] = post;
           } else {
+            updatedPosts.pop()
             updatedPosts.unshift(post);
           }
           return {
