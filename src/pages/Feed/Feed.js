bcfe860ff9eac68b9fa90ebf38fc9638318a5db2
Pranjali Yadav <51490690+Pranjaliyadav@users.noreply.github.com>
2024-12-10 18:05:03 +0530

    chnges

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -203,14 +203,23 @@ class Feed extends Component {
 
         }
         this.setState(prevState => {
-          
+          let updatedPosts = [...prevState.posts];
+          if (prevState.editPost) {
+            const postIndex = prevState.posts.findIndex(
+              p => p._id === prevState.editPost._id
+            );
+            updatedPosts[postIndex] = post;
+          } else {
+            updatedPosts.unshift(post);
+          }
           return {
-            // posts: updatedPosts,
+            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false
           };
         });
+        
       })
       .catch(err => {
         console.log(err);
